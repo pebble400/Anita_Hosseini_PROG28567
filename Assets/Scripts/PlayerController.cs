12341bbbe9b372966771688663786a8c257f9303
anitahosseini37@gmail.com <anitahosseini37@gmail.com>
2025-11-16 18:08:03 -0500

    Finished Jounral 6

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float move;
+    private float speed = 5f;
+    Vector2 boxSize = new Vector2 (1, 1); // setting the box size for the box cast
+    public float distance; //Distance of the raycast, set in the inspector 
+    public LayerMask groundLayer;
     public enum FacingDirection
     {
         left, right
@@ -23,20 +28,45 @@ public class PlayerController : MonoBehaviour
 
     private void MovementUpdate(Vector2 playerInput)
     {
-
+        move = Input.GetAxisRaw("Horizontal");
+        transform.Translate(new Vector3(move * speed * Time.deltaTime, 0f, 0f));
     }
 
     public bool IsWalking()
     {
-        return false;
+        if(move == 1 || move == -1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+           
     }
     public bool IsGrounded()
     {
-        return false;
+        
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, distance, groundLayer)) //getting access to the physics class and passing the starting point for the raycast
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        } 
     }
 
     public FacingDirection GetFacingDirection()
     {
+        if (move == 1)
+        {
+            return FacingDirection.right;
+        }
+        if (move == -1)
+        {
+            return FacingDirection.left;
+        }
         return FacingDirection.left;
     }
 }
